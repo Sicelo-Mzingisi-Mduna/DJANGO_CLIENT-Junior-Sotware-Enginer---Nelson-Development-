5c83e849331fff23e4bf3d3fbc24784a122071e8
Sicelo Mduna <siceloit@gmail.com>
2025-06-17 22:12:02 +0200

    Final changes

--- client/client/webapp/views.py
+++ client/client/webapp/views.py
@@ -5,6 +5,8 @@ from django.http import HttpRequest
 
 def home(request: HttpRequest):
     response_data = None
+    status_code = None
+
     if request.method == 'POST':
         form = ValidationForm(request.POST)
         if form.is_valid():
@@ -16,12 +18,16 @@ def home(request: HttpRequest):
             try:
                 response = requests.post(constructed_url)
                 response_data = response.json()
+                status_code = response.status_code
             except Exception as e:
                 response_data = {"error": str(e)}
+                status_code = 500  # Assume failure
     else:
         form = ValidationForm()
     
     return render(request, 'webapp/index.html', {
         'form': form,
-        'response_data': response_data
+        'response_data': response_data,
+        'status_code': status_code
     })
+
